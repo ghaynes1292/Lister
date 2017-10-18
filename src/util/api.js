607558e0df1a4d02b67d8048474d8de152e62fb2
Gavin Haynes <gavin.haynes@moove-it.com>
2017-10-18 16:43:08 -0500

    Version 0.9.1

--- src/util/api.js
+++ src/util/api.js
@@ -2,7 +2,7 @@ export const apiFetchSuggestions = (text) => {
   const lowerCase = text.toLowerCase()
   return new Promise((resolve, reject) => {
     const xmlhttp = new XMLHttpRequest();
-    const url = `http://cors-proxy.htmldriven.com/?url=https://sg.media-imdb.com/suggests/${lowerCase.charAt(0)}/${lowerCase}.json`;
+    const url = `https://cors-proxy.htmldriven.com/?url=https://sg.media-imdb.com/suggests/${lowerCase.charAt(0)}/${lowerCase}.json`;
 
     xmlhttp.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
