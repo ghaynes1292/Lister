01619350ba439ea2c6e47b0f010b383c1e3d18e9
Gavin Haynes <gavin.haynes@moove-it.com>
2017-10-27 11:49:56 -0500

    Fix filtering list items

--- src/util/filter.js
+++ src/util/filter.js
@@ -1,7 +1,7 @@
 export const ADDED = 'createdAt';
 export const WATCHED = 'attributes.watchDate';
-export const TITLE = 'attributes.title';
-export const RELEASE = 'attributes.year';
+export const TITLE = 'attributes.Title';
+export const RELEASE = 'attributes.Released';
 
 export const ASCENDING = 'ASCENDING';
 export const DESCENDING = 'DESCENDING';
